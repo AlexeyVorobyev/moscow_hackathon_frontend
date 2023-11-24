05ce33150f611499010e77de050f44d494ab86f4
a.vorobyev <mister.alex49@yandex.ru>
2023-11-24 19:56:06 +0300

    fixes

--- src/components/skeleton/SkeletonWrapper.tsx
+++ src/components/skeleton/SkeletonWrapper.tsx
@@ -1,17 +1,27 @@
-import {FC, ReactNode} from "react";
-import {Box, Stack, Typography} from "@mui/material";
-import {SideNavigation} from "./SideBar/SideNavigation";
-import {theme} from "../Theme/theme";
-import {PageWrapper} from "./PageWrapper";
-import {ErrorBoundary} from "./ErrorBoundary/ErrorBoundary";
-import {PROJECT_NAME} from "../../globalVars";
+import React, {FC, ReactNode, useState} from 'react'
+import {Box, Button, IconButton, Paper, Popover, Stack, Typography} from '@mui/material'
+import {SideNavigation} from './SideBar/SideNavigation'
+import {theme} from '../Theme/theme'
+import {PageWrapper} from './PageWrapper'
+import {ErrorBoundary} from './ErrorBoundary/ErrorBoundary'
+import {PROJECT_NAME} from '../../globalVars'
 import headerLogoIcon from './../../assets/logo/headerLogo.svg'
+import CloseIcon from '@mui/icons-material/Close'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import {LinkRouterWrapper} from '../LinkRouterWrapper/LinkRouterWrapper'
 
 interface IProps {
     children: ReactNode
 }
 
 export const SkeletonWrapper: FC<IProps> = ({children}) => {
+
+    const [openButtonPopover, setOpenButtonPopover] = useState<boolean>(false)
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+        setOpenButtonPopover(!openButtonPopover)
+    }
 
     return (
         <Box width={'100%'} height={'100%'}>
@@ -20,12 +30,56 @@ export const SkeletonWrapper: FC<IProps> = ({children}) => {
                 boxSizing: 'border-box',
                 padding: theme.spacing(2),
                 boxShadow: 2,
-                zIndex:1000,
-                position:'relative'
+                zIndex: 1000,
+                position: 'relative'
             }}>
-                <Stack alignItems={'center'} direction={'row'} height={'100%'}>
+                <Stack alignItems={'center'} direction={'row'} height={'100%'} justifyContent={'space-between'}>
                     <img src={headerLogoIcon} alt={PROJECT_NAME}/>
-                    {/*<Typography variant={'h3'} color={theme.palette.primary.contrastText}>{PROJECT_NAME}</Typography>*/}
+                    <Stack direction={'row'} alignItems={'center'} spacing={1}>
+                        <LinkRouterWrapper to={'/cabinet'}>
+                            <IconButton>
+                                <AccountCircleIcon style={{color:theme.palette.primary.contrastText}}/>
+                            </IconButton>
+                        </LinkRouterWrapper>
+                        <LinkRouterWrapper to={'/cabinet'}>
+                            <Typography variant={'subtitle1'} sx={{cursor: 'pointer'}} color={theme.palette.primary.contrastText}>Личный кабинет</Typography>
+                        </LinkRouterWrapper>
+                        <IconButton onClick={handleClick}>
+                            <CloseIcon style={{color:theme.palette.primary.contrastText}}/>
+                        </IconButton>
+                        <Popover
+                            open={openButtonPopover}
+                            anchorEl={anchorEl}
+                            sx={{
+                                zIndex: 100000
+                            }}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                            }}
+                        >
+                            <Paper elevation={3} sx={{padding: '10px'}}>
+                                <Stack direction={'column'} spacing={1}>
+                                    <Typography>Выйти из аккаунта?</Typography>
+                                    <Stack direction={'row'} spacing={'10px'}>
+                                        <Button size={'large'}
+                                                onClick={() => {
+                                                    localStorage.clear()
+                                                    location.reload()
+                                                }}
+                                                variant="contained">
+                                            Да
+                                        </Button>
+                                        <Button size={'large'}
+                                                onClick={() => setOpenButtonPopover(false)}
+                                                variant="contained">
+                                            Нет
+                                        </Button>
+                                    </Stack>
+                                </Stack>
+                            </Paper>
+                        </Popover>
+                    </Stack>
                 </Stack>
             </Box>
             <Stack height={'calc(100vh - 70px)'} width={'100%'} direction={'row'}>
--- src/redux/api/jwtMiddleware.ts
+++ src/redux/api/jwtMiddleware.ts
@@ -3,57 +3,24 @@ import {isRejectedWithValue} from '@reduxjs/toolkit'
 import {api} from './api'
 import {ILoginResponse, IRefreshPayload} from './types/auth'
 import {getTokens} from '../../components/functions/getAuthToken'
-import {userSlice} from '../store/user/user.slice'
 
 export const jwtMiddleware: Middleware =
     (state) => (next: any) => (action: any) => {
-        // console.log(action)
-        // if (action && action.type === 'api/executeQuery/pending') {
-        //     if (!localStorage.getItem('expiry')) return next(action)
-        //     if (parseInt(localStorage.getItem('expiry')!) > Date.now()) return next(action)
-        //
-        //     localStorage.clear()
-        //     state.dispatch(userSlice.actions.setLogin(false))
-        //
-        //     // state.dispatch(
-        //     //     // @ts-ignore       `           `
-        //     //     api.endpoints.refresh.initiate({refreshToken: getTokens().refreshToken} as IRefreshPayload)
-        //     // )
-        // }
-        // else {
-        //     return
-        // }
-        // if (action && action.type === 'api/executeQuery/pending') {
-        //     if (parseInt(localStorage.getItem('expiry')!) > Date.now() + 5000) {
-        //         return next(action)
-        //     }
-        //
-        // }
-        // else {
-        //     return next(action)
-        // }
         if (action && isRejectedWithValue(action)) {
-            // Catch the authorization error and refresh the tokens
             console.warn('We got a rejected action!', action.payload.status)
             if (action.payload.status === 401) {
-
                 state.dispatch(
-                    // @ts-ignore       `           `
+                    // @ts-ignore
                     api.endpoints.refresh.initiate({refreshToken: getTokens().refreshToken} as IRefreshPayload)
-                ).then((response:any) => {
-                    console.log(response,'lolol')
+                ).then((response: any) => {
                     if (response.hasOwnProperty('error')) {
                         localStorage.clear()
-                    }
-                    else {
-                        if (response.data) {
-                            const res = response.data as ILoginResponse
-                            console.log(res,'lolol')
-                            localStorage.setItem('accessToken', res.response.accessToken)
-                            localStorage.setItem('refreshToken', res.response.refreshToken)
-                            localStorage.setItem('expiry', res.response.expiry.toString())
-                            location.reload()
-                        }
+                    } else if (response.data) {
+                        const res = response.data as ILoginResponse
+                        localStorage.setItem('accessToken', res.response.accessToken)
+                        localStorage.setItem('refreshToken', res.response.refreshToken)
+                        localStorage.setItem('expiry', res.response.expiry.toString())
+                        location.reload()
                     }
                 })
             }
